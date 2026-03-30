00785ca655f990f5cf27fa9349abf58b8ccb0e3a
williammaina <wmaina8442@gmail.com>
2026-03-30 14:21:59 +0300

    added the funtions to perfom invetory management adding products, indexing the first element ad removing the last product

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -3,6 +3,30 @@
 
 
 // Export the necessary parts for testing
+let products = ["laptop","phone","Headphones","Monitor"];
+
+function logFirstProduct(arr) {
+  if (arr.length > 0) {
+    console.log(arr[0]);
+  } else {
+    console.log("The array is empty!");
+  }
+}
+logFirstProduct(products)
+
+function addProduct (arr, newProduct) {
+  arr.push (newProduct);
+  console.log(`${newProduct}`);
+  }
+addProduct(products,"camera");
+console.log (products)
+function removeLastProduct (arr, removedProduct){
+  arr.pop (removedProduct);
+  console.log (`${removedProduct}`);
+}
+removeLastProduct(products,"camera")
+console.log (products)
+
 module.exports = {
   logFirstProduct: typeof logFirstProduct !== 'undefined' ? logFirstProduct : undefined,
   addProduct: typeof addProduct !== 'undefined' ? addProduct : undefined,
@@ -10,3 +34,5 @@ module.exports = {
   removeLastProduct: typeof removeLastProduct !== 'undefined' ? removeLastProduct : undefined,
   products
 };
+
+
